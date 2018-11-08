59707f36b8ef768c861e80aa28e568002c37c700
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-08 15:04:16 +0300

    Initial commit

---
+++ TestsGeneratorTestsClass/TestsGeneratorTestsClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsGeneratorUnitTests
+{
+    [TestClass]
+    public class TestsGeneratorTestsClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
