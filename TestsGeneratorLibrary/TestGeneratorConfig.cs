80e87d8013510f2fdca19038f32d10e1975840f9
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-08 22:19:46 +0300

    Added generatorConfig

---
+++ TestsGeneratorLibrary/TestGeneratorConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    public class TestGeneratorConfig
+    {
+        public int MaxProcessingTasksCount { get; }
+        public int MaxWritingTasksCount { get; }
+
+        public TestGeneratorConfig(int maxProcessingTasksCount, int maxWritingTasksCount)
+        {
+            MaxProcessingTasksCount = maxProcessingTasksCount;
+            MaxWritingTasksCount = maxWritingTasksCount;
+        }
+    }
+}
