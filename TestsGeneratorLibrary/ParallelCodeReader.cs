8be7b95d66f4f4377d79b7858389c2e5068e485d
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-08 22:14:46 +0300

    Completed codeReader and templateGenerator

---
+++ TestsGeneratorLibrary/ParallelCodeReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    public class ParallelCodeReader
+    {
+        private readonly IEnumerable<string> _filePaths;
+        private readonly ParallelOptions _maxReadingTasksCount;
+
+        public ParallelCodeReader(IEnumerable<string> filePaths, int maxReadingTasksCount)
+        {
+            _filePaths = filePaths;
+
+            _maxReadingTasksCount = new ParallelOptions { MaxDegreeOfParallelism = maxReadingTasksCount };
+        }
+
+        public IEnumerable<string> Provide()
+        {
+            List<string> codeBuffer = new List<string>();
+            List<Exception> exceptions = new List<Exception>();
+
+            Parallel.ForEach(
+                _filePaths, 
+                _maxReadingTasksCount, 
+                filePath =>
+                {
+                    try
+                    {
+                        codeBuffer.Add(File.ReadAllText(filePath));
+                    }
+                    catch (Exception exception)
+                    {
+                        exceptions.Add(exception);
+                    }
+                });
+
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+
+            return codeBuffer;
+        }
+    }
+}
