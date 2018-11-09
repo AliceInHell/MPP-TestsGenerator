351ed9a4d11994e1668811b0af2774507a84eeef
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-09 17:31:27 +0300

    Added resultWriter (CodeWriter)

---
+++ TestsGeneratorLibrary/CodeWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TestsGeneratorLibrary.Structures;
+
+namespace TestsGeneratorLibrary
+{
+    public class ParallelCodeWriter
+    {
+        private readonly string _outputDirectoryPath;
+
+        public ParallelCodeWriter(string outputDirectoryPath)
+        {
+            _outputDirectoryPath = outputDirectoryPath;
+
+            if (!Directory.Exists(_outputDirectoryPath))
+            {
+                Directory.CreateDirectory(_outputDirectoryPath);
+            }
+        }
+
+        public void Consume(GeneratedTestClass @class)
+        {
+            string filePath = filePath = $"{_outputDirectoryPath}\\{@class.TestClassName}";
+
+            Exception error = null;
+            try
+            {
+                File.WriteAllText(filePath, @class.TestClassData);
+            }
+            catch (Exception exception)
+            {
+                error = exception;
+            }
+        }
+    }
+}
