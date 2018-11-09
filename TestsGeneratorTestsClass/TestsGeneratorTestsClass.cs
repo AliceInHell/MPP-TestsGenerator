0abea2cd48f78695dd9606e3e773864a6bec52db
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-09 18:12:08 +0300

    Added tests initialization to our unitTests

--- TestsGeneratorTestsClass/TestsGeneratorTestsClass.cs
+++ TestsGeneratorTestsClass/TestsGeneratorTestsClass.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestsGeneratorLibrary;
 
 namespace TestsGeneratorUnitTests
 {
     [TestClass]
     public class TestsGeneratorTestsClass
     {
+        private int readingTasksCount;
+        private int writingTasksCount;
+
+        private TestGeneratorConfig config;
+
+        private string outputDirectory;
+        private List<string> paths;
+
+        private ParallelCodeReader reader;
+        private CodeWriter writer;
+
+        private TestsGenerator generator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            readingTasksCount = 3;
+            writingTasksCount = 3;
+            config = new TestGeneratorConfig(readingTasksCount, writingTasksCount);
+
+            outputDirectory = @"./";
+
+            paths = new List<string>();
+            paths.Add(@"./someClass.cs");
+
+            reader = new ParallelCodeReader(paths, readingTasksCount);
+            writer = new CodeWriter(outputDirectory);
+
+            generator = new TestsGenerator(config);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
